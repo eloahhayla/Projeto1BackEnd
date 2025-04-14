244fcaa34e31c510bcba2e1b7579eefe8a6dac75
Eloah <eloah.ferreira.silva@escola.pr.gov.br>
2025-04-14 12:19:38 -0300

    adiciona livro

---
+++ aula10-lacoFor.js
@@ -0,0 +1,17 @@
+// 1) Crie um laço FOR que mostra a tabuada (de 1 a 10)
+// do número armazenado na varíavel abaixo:
+let numero = 3;
+for(let num = 1; num <=10; num++){
+    console.log(numero + " X " + num + " = " + numero*num)
+}
+// 2) Crie um programa que recebe a idade de 10 pessoas,
+// conforme o array abaixo, e mostre a porcentagem de pessoas
+// maiores de 18 anos.
+let idades = [10, 35, 15, 20, 27, 17, 18, 54, 65, 12];
+
+// 3) Crie um programa que imprime um quadrado com *,
+// de acordo com o número armazenado na variável:
+let tamanho_quadrado = 3;
+
+// 4) Altere o código da questão 3 para gerar um triângulo.
+let tamanho_triangulo1 = 3;
--- aula9-exercicio3.js
+++ aula9-exercicio3.js
@@ -11,6 +11,30 @@ function mostralivros(){
     })
 }
 
+function adicionaLivro(titulo, autor, editora, ano, sinopse, paginas, genero, preco){
+    livros.push({
+        titulo: titulo,
+        autor: autor,
+        editora: editora,
+        ano: ano,
+        sinopse: sinopse,
+        paginas: paginas,
+        genero: genero,
+        preco: preco
+    });
+    console.log("Livro adicionado com sucesso!")
+}
+
+adicionaLivro(
+    "A garota que bebeu a lua", 
+    "Kelly Barnhill", 
+    "Galera", 
+    2018, 
+    "Uma fábula encantadora sobre aceitação, amor e amadurecimento. Todo ano, o povo do Protetorado deixa um bebê como oferenda para a bruxa da floresta, acreditando que isso os protegerá. No entanto, a bruxa, chamada Xan, é bondosa e cuida dos bebês, alimentando-os com luz estelar. Em uma ocasião, ela acidentalmente dá luz do luar a uma menina, dotando-a de magia extraordinária. A menina, chamada Luna, cresce e descobre seus poderes, enfrentando desafios que podem mudar seu destino e o de todos ao seu redor", 
+    308, 
+    ["fantasia", "literatura juvenil"], 
+    45.01);
+
 //json = JavaScript Object Notation
 //Gera  um arquivo json dos livros
 function criarArquivo(){
